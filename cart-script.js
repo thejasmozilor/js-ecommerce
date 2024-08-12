98aa006115777b7076197ea1f51ac95d75f59f2e
Thejas T <Thejas@LAP083-Thejas-T.local>
2024-08-12 17:33:57 +0530

    completed

--- cart-script.js
+++ cart-script.js
@@ -11,100 +11,121 @@ let total = 0;
 let subtotal = 0;
 let eachItemtotal = 0;
 
-cart.forEach((item) => {
-  const newCartProduct = cartProduct.cloneNode(true);
-  console.log(item.price);
+const renderCart = () => {
+  cartProducts.innerHTML = "";
+  cart.forEach((item) => {
+    const newCartProduct = cartProduct.cloneNode(true);
+    // console.log(item.price);
 
-  newCartProduct.style.display = "block";
+    newCartProduct.style.display = "block";
 
-  newCartProduct.querySelector(".cart-product-img").src = item.image;
-  newCartProduct.querySelector(".cart-product-name").textContent = item.name;
-  newCartProduct.querySelector(".cart-product-price").textContent = item.price;
-  newCartProduct.querySelector(".quantity-no").textContent = item.quantity;
-  eachItemtotal = parseFloat(item.quantity) * parseFloat(item.price);
-  newCartProduct.querySelector(".cart-product-subtotal").textContent =
-    eachItemtotal;
-  subtotal += eachItemtotal;
-  cartProducts.append(newCartProduct);
-});
+    newCartProduct.querySelector(".cart-product-img").src = item.image;
+    newCartProduct.querySelector(".cart-product-name").textContent = item.name;
+    newCartProduct.querySelector(".cart-product-price").textContent =
+      item.price;
+    newCartProduct.querySelector(".quantity-no").textContent = item.quantity;
+    eachItemtotal = parseFloat(item.quantity) * parseFloat(item.price);
+    newCartProduct.querySelector(".cart-product-subtotal").textContent =
+      eachItemtotal;
+    subtotal += eachItemtotal;
+    cartProducts.append(newCartProduct);
+  });
+};
+
+renderCart();
 
 cartProducts.addEventListener("click", (event) => {
+  if (event.target.classList.contains("cart-product-remove")) {
+    const productName = event.target
+      .closest(".cart-product")
+      .querySelector(".cart-product-name").textContent;
+
+    const itemIndex = cart.findIndex((item) => item.name === productName);
+
+    if (itemIndex > -1) {
+      alert(`${cart[itemIndex].name} has been removed from cart`);
+      cart.splice(itemIndex, 1);
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+    renderCart();
+    cartTotal();
+    updateCartItemCount();
+  }
+
   console.log(event);
   if (event.target.classList.contains("minus")) {
-    console.log("jhbkuj");
     quantity = parseInt(event.target.nextElementSibling.textContent);
     if (quantity > 0) {
       quantity--;
-      console.log(quantity);
       event.target.nextElementSibling.textContent = quantity;
+
+      const productName = event.target
+        .closest(".cart-product")
+        .querySelector(".cart-product-name").textContent;
+
       const itemIndex = cart.findIndex((item) => item.name === productName);
+
       if (itemIndex > -1) {
-        cart[itemIndex].quantity = quantity;
+        if (quantity === 0) {
+          alert(`${cart[itemIndex].name} has been removed from cart`);
+          cart.splice(itemIndex, 1);
+        } else {
+          cart[itemIndex].quantity = quantity;
+        }
         localStorage.setItem("cart", JSON.stringify(cart));
       }
-
+      renderCart();
       cartTotal();
       updateCartItemCount();
     }
   } else if (event.target.classList.contains("plus")) {
-    console.log("jhbkuj");
     quantity = parseInt(event.target.previousElementSibling.textContent);
     quantity++;
-    console.log(quantity);
     event.target.previousElementSibling.textContent = quantity;
-    localStorage.setItem("cart", JSON.stringify(cart));
+
+    const productName = event.target
+      .closest(".cart-product")
+      .querySelector(".cart-product-name").textContent;
+
+    console.log("productname", productName);
+
+    const itemIndex = cart.findIndex((item) => item.name === productName);
+    if (itemIndex > -1) {
+      cart[itemIndex].quantity = quantity;
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
     cartTotal();
     updateCartItemCount();
   }
 });
 
-// const minus = document.querySelector(".minus");
-// const plus = document.querySelector(".plus");
-// const quantityNo = document.querySelector(".quantity-no");
-
-// plus.addEventListener("click", () => {
-//   quantity++;
-//   quantityNo.textContent = quantity;
-// });
-
-// minus.addEventListener("click", () => {
-//   if (quantity > 0) {
-//     quantity--;
-//     quantityNo.textContent = quantity;
-//   }
-// });
-
 const cartTotal = () => {
   subtotal = 0;
-  console.log("hoihoi");
   items = cartProducts.querySelectorAll(".cart-product-container");
   items.forEach((item) => {
-    console.log(item.querySelector(".cart-product-price").textContent);
-
     item.querySelector(".cart-product-subtotal").textContent = parseFloat(
       item.querySelector(".cart-product-price").textContent *
         item.querySelector(".quantity-no").textContent
     ).toFixed(2);
 
-    console.log(
-      "subtotal",
-      item.querySelector(".cart-product-subtotal").textContent
-    );
-
     subtotal += parseFloat(
       item.querySelector(".cart-product-subtotal").textContent
     );
-    console.log(subtotal);
   });
 
-  document.querySelector(".sub-total").textContent = subtotal;
+  document.querySelector(".sub-total").textContent = subtotal.toFixed(2);
 
   const tax = parseFloat((subtotal / 100) * 8).toFixed(2);
 
   document.querySelector(".tax").textContent = tax;
   discount = parseFloat(document.querySelector(".discount").textContent);
 
-  const totalAmount = parseFloat(subtotal) + parseFloat(tax) - discount;
+  const totalAmount = (
+    parseFloat(subtotal) +
+    parseFloat(tax) -
+    discount
+  ).toFixed(2);
   document.querySelector(".total-amount").textContent = totalAmount;
 };
 
@@ -117,3 +138,9 @@ const updateCartItemCount = () => {
 };
 
 updateCartItemCount();
+
+const returnToShop = document.querySelector(".return");
+
+returnToShop.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
